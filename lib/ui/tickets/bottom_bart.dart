3c631f694778b28ee7081029dcc9fa0c978fa757
genix <intergenixyt@gmail.com>
2019-11-30 11:58:30 +0100

    Add dragging logic.

--- lib/ui/tickets/bottom_bart.dart
+++ lib/ui/tickets/bottom_bart.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -53,21 +54,62 @@ class _BottomBarState extends State<BottomBar>
         left: 0,
         right: 0,
         bottom: 0,
-        child: GestureDetector(
-          onTap: _toggle,
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 32),
-            decoration: BoxDecoration(
-              color: Colors.grey[850],
-              borderRadius: BorderRadius.vertical(top: Radius.circular(32)),
-            ),
-          ),
+        child: _buildContainer(),
+      ),
+    );
+  }
+
+  // WIDGETS
+
+  Widget _buildContainer() {
+    return GestureDetector(
+      onTap: _toggle,
+      onVerticalDragUpdate: _handleDragUpdate,
+      onVerticalDragEnd: _handleDragEnd,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 32),
+        decoration: BoxDecoration(
+          color: Colors.grey[850],
+          borderRadius: BorderRadius.vertical(top: Radius.circular(32)),
         ),
       ),
     );
   }
 
   // FUNCTIONS
+
+  /// The 'details' object provides the position of the touch and the distance
+  /// it has traveled since the last update.
+  void _handleDragUpdate(DragUpdateDetails details) {
+    // Update the _anim by the movement done by user.
+    _anim.value -= details.primaryDelta / maxHeight;
+  }
+
+  /// The velocity at which the pointer was moving when it stopped contacting
+  /// the screen is available in the 'details'.
+  void _handleDragEnd(DragEndDetails details) {
+    if (_anim.isAnimating || _anim.status == AnimationStatus.completed) {
+      return;
+    }
+
+    // calculate the velocity of the gesture
+    final double velocity = details.velocity.pixelsPerSecond.dy / maxHeight;
+
+    if (velocity < 0.0) {
+      // either continue it upwards
+      _anim.fling(velocity: math.max(2.0, -velocity));
+
+      //
+    } else if (velocity > 0.0) {
+      // or continue it downwards
+      _anim.fling(velocity: math.min(-2.0, -velocity));
+
+      //
+    } else {
+      // or just continue to whichever edge is closer
+      _anim.fling(velocity: _anim.value < 0.5 ? -2.0 : 2.0);
+    }
+  }
 
   void _toggle() {
     final bool isOpen = (_anim.status == AnimationStatus.completed);
