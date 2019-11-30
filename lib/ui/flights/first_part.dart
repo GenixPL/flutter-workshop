6468b910dada89169694ec9519d91c06938e1430
genix <intergenixyt@gmail.com>
2019-11-30 22:03:10 +0100

    Add rest of first part.

--- lib/ui/flights/first_part.dart
+++ lib/ui/flights/first_part.dart
@@ -9,19 +9,19 @@ class FirstPart extends StatelessWidget {
         children: <Widget>[
           Expanded(
             child: Container(
-              color: Colors.redAccent, // to remove
+              color: Colors.white, // to remove
               child: Column(
                 children: <Widget>[
                   Expanded(
                     flex: 4,
                     child: Container(
-                      color: Colors.lightBlue, // to remove
+                      color: Colors.white, // to remove
                       child: Row(
                         children: <Widget>[
                           Expanded(
                             flex: 6,
                             child: Container(
-                              color: Colors.amber, // to remove
+                              color: Colors.white, // to remove
                               child: Column(
                                 children: <Widget>[
                                   Expanded(
@@ -59,8 +59,24 @@ class FirstPart extends StatelessWidget {
                           Expanded(
                             flex: 2,
                             child: Container(
-                              color: Colors.amberAccent, // to remove
-                              child: Column(),
+                              color: Colors.white, // to remove
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: <Widget>[
+                                  Expanded(
+                                    flex: 8,
+                                    child: Container(),
+                                  ),
+                                  Icon(
+                                    Icons.add_circle_outline,
+                                    color: Colors.grey,
+                                  ),
+                                  Expanded(
+                                    flex: 3,
+                                    child: Container(),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         ],
@@ -70,15 +86,21 @@ class FirstPart extends StatelessWidget {
                   Expanded(
                     flex: 1,
                     child: Container(
-                      color: Colors.blue, // to remove
-                      child: Row(),
+                      color: Colors.white, // to remove
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: _buildDoubleRow(),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                   Expanded(
                     flex: 2,
                     child: Container(
-                      color: Colors.blueAccent, // to remove
-                      child: Row(),
+                      color: Colors.white, // to remove
+                      child: _buildFab(),
                     ),
                   ),
                 ],
@@ -98,15 +120,14 @@ class FirstPart extends StatelessWidget {
     return Row(
       crossAxisAlignment: CrossAxisAlignment.end,
       children: <Widget>[
-        Expanded(
-          flex: 3,
+        Padding(
+          padding: const EdgeInsets.only(left: 24, right: 24),
           child: Icon(
             iconData,
             color: Colors.redAccent,
           ),
         ),
         Expanded(
-          flex: 7,
           child: TextField(
             controller: TextEditingController.fromValue(
               TextEditingValue(text: content),
@@ -114,6 +135,82 @@ class FirstPart extends StatelessWidget {
             decoration: InputDecoration(
               labelText: label,
             ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDoubleRow() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 24, right: 24),
+          child: Icon(
+            Icons.date_range,
+            color: Colors.redAccent,
+          ),
+        ),
+        Expanded(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                flex: 7,
+                child: TextField(
+                  controller: TextEditingController.fromValue(
+                    TextEditingValue(text: '29 June 2017'),
+                  ),
+                  decoration: InputDecoration(
+                    labelText: 'Departure',
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(),
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                flex: 7,
+                child: TextField(
+                  controller: TextEditingController.fromValue(
+                    TextEditingValue(
+                      text: '29 July 2017', // wtf - monthly flight?
+                    ),
+                  ),
+                  decoration: InputDecoration(
+                    labelText: 'Departure',
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildFab() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(bottom: 16),
+          child: FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: Colors.redAccent,
+            child: Icon(Icons.timeline),
           ),
         ),
       ],
