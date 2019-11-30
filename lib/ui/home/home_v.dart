8b26b1a15a22daa29cbfa3f7cde70a2fb8a6ba48
genix <intergenixyt@gmail.com>
2019-11-30 11:05:02 +0100

    update home

--- lib/ui/home/home_v.dart
+++ lib/ui/home/home_v.dart
@@ -2,17 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:workshop/ui/base/base_view.dart';
 import 'package:workshop/ui/home/home_vm.dart';
 
-
 class HomeV extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BaseView<HomeVM>(
-      onModelReady: (model) async {
-      },
       builder: (context, model, child) => Scaffold(
-        appBar: AppBar(title:Text('Home')),
-        body: Center(child: Text('Home'),)
+        appBar: AppBar(title: Text('Home')),
+        body: _buildBody(),
       ),
+    );
+  }
+
+  Widget _buildBody() {
+    return Center(
+      child: Text('Home'),
     );
   }
 }
