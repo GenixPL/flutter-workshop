c3f8a0241e948f2e310d4ccbc2aaf7bf13074b20
genix <intergenixyt@gmail.com>
2019-11-30 20:35:35 +0100

    Add buttons to top bar.

--- lib/ui/flights/top_bar.dart
+++ lib/ui/flights/top_bar.dart
@@ -16,6 +16,82 @@ class TopBarFlights extends StatelessWidget {
           ],
         ),
       ),
+      child: _buildContent(context),
     );
   }
+
+  Widget _buildContent(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.3 / 2,
+      child: Column(
+        children: <Widget>[
+          Text(
+            'AirAsia',
+            style: TextStyle(
+              fontFamily: 'Yellowtail',
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              SizedBox(width: 16),
+              Expanded(
+                child: _buildEmptyButton('ONE WAY'),
+              ),
+              SizedBox(width: 8),
+              Expanded(
+                child: _buildEmptyButton('ROUND'),
+              ),
+              SizedBox(width: 8),
+              Expanded(
+                child: _buildFilledButton('MULTICITY'),
+              ),
+              SizedBox(width: 16),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildEmptyButton(String text) {
+    return OutlineButton(
+      onPressed: () {},
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      borderSide: BorderSide(
+        color: Colors.white,
+        width: 2,
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(32),
+      ),
+    );
+  }
+
+  Widget _buildFilledButton(String text) {
+    return RaisedButton(
+      onPressed: () {},
+      color: Colors.white,
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.redAccent,
+        ),
+      ),
+      elevation: 0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(32),
+      ),
+    );
+  }
+
+  // VALUES
+
 }
