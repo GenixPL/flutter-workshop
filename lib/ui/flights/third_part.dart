5cb79a0b87aca6a84b8b7d1d5ae6b30c7722c402
genix <intergenixyt@gmail.com>
2019-12-01 16:07:23 +0100

    add another fab

--- lib/ui/flights/third_part.dart
+++ lib/ui/flights/third_part.dart
@@ -1,6 +1,38 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class ThirdPart extends StatelessWidget {
+class ThirdPart extends StatefulWidget {
+  @override
+  _ThirdPartState createState() => _ThirdPartState();
+}
+
+class _ThirdPartState extends State<ThirdPart> with TickerProviderStateMixin {
+  AnimationController _fabController;
+  Animation _fabAnimation;
+
+  @override
+  initState() {
+    super.initState();
+
+    _fabController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 250),
+    );
+
+    _fabAnimation = Tween(
+      begin: pi * 0.25,
+      end: 0.0,
+    ).animate(_fabController);
+  }
+
+  @override
+  dispose() {
+    _fabController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -10,32 +42,49 @@ class ThirdPart extends StatelessWidget {
         height: MediaQuery.of(context).size.height * 0.85,
         width: MediaQuery.of(context).size.width,
         child: Column(
-          children: <Widget>[],
+          children: <Widget>[
+            Expanded(child: _buildList()),
+            _buildFab(),
+          ],
         ),
       ),
     );
   }
 
-  // Padding(
-  //                     padding: const EdgeInsets.only(bottom: 16),
-  //                     child: Align(
-  //                       alignment: Alignment.bottomCenter,
-  //                       child: FloatingActionButton(
-  //                         elevation: 0,
-  //                         backgroundColor: Colors.redAccent,
-  //                         child: AnimatedBuilder(
-  //                           animation: _fabAnimation,
-  //                           child: Icon(
-  //                             Icons.fingerprint,
-  //                             color: Colors.white,
-  //                           ),
-  //                           builder: (context, child) => Transform.rotate(
-  //                             angle: _fabAnimation.value,
-  //                             child: child,
-  //                           ),
-  //                         ),
-  //                         onPressed: () => _fabController.forward(),
-  //                       ),
-  //                     ),
-  //                   ),
+  Widget _buildList() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text('A'),
+        Text('A'),
+        Text('A'),
+        Text('A'),
+      ],
+    );
+  }
+
+  Widget _buildFab() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16),
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: FloatingActionButton(
+          elevation: 0,
+          backgroundColor: Colors.redAccent,
+          child: AnimatedBuilder(
+            animation: _fabAnimation,
+            child: Icon(
+              Icons.fingerprint,
+              color: Colors.white,
+            ),
+            builder: (context, child) => Transform.rotate(
+              angle: _fabAnimation.value,
+              child: child,
+            ),
+          ),
+          onPressed: () => _fabController.forward(),
+        ),
+      ),
+    );
+  }
 }
